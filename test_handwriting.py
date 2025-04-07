6f93559e3f6dda05b11b32908de7dfd78f48db49
Akshay Viswan <akshayviswan007@gmail.com>
2025-04-07 13:21:41 +0530

    Configure two files

--- test_handwriting.py
+++ test_handwriting.py
@@ -2,4 +2,4 @@ from transformers import pipeline
 
 pipe = pipeline("image-classification", "gianlab/swin-tiny-patch4-window7-224-finetuned-parkinson-classification")
 
-print(pipe("data\pd-hw-healthy\pd-hw-healthy\V01PE01.png"))
+print(pipe(r"data\image_data\pd-hw-parkinson\pd-hw-parkinson\V03PE04.png"))
